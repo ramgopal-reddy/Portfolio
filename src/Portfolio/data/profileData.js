f5d022ce78cc2c8488ed6cfa852e545120bf1e77
RamGopal-Reddy <redabothularamgopalreddy@gmail.com>
2025-11-08 09:38:54 +0530

    Update profileData.js

--- src/Portfolio/data/profileData.js
+++ src/Portfolio/data/profileData.js
@@ -4,7 +4,7 @@ export const profileData = [
     role: "AI/ML Researcher and Software Developer",
     bio: "Passionate about leveraging AI and machine learning to solve complex problems and create innovative solutions.",
     location: "Hyderabad, India",
-    email: "work@email.com",
+    email: "redabothularamgopalreddy@gmail.com",
     linkedIn: "https://www.linkedin.com/in/ramgopal-reddy",
     github: "https://www.github.com/ramgopal-reddy",
     website: "https://www.ramgopalreddy.xyz",
